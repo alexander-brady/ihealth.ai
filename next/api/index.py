d00feb0fd007a5c1240ec21c4a78be59cca6b259
alexander-brady <bradya@student.ethz.ch>
2024-09-21 10:30:56 -0400

    updated query api

--- next/api/index.py
+++ next/api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/api/query")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/query", methods=["POST"])
+def post_query():
+    data = request.json  
+    query = data.get('query')
+    return jsonify({"response": "Hello World"})
